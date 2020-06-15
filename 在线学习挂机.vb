d6a85fa4a4f0139c52168883d85093a527a47301
BugHub <31329925+abingx@users.noreply.github.com>
2020-06-15 16:26:35 +0800

    Update 在线学习挂机.vb

--- 在线学习挂机.vb
+++ 在线学习挂机.vb
@@ -1,4 +1,4 @@
-//Veison 3.2
+//Veison 4.0
 Dim MyArray   //定义变量，用于获取窗口大小
 Dim x1, y1, x2, y2   //定义窗口坐标变量
 Dim intX, intY    //定义坐标变量
@@ -10,6 +10,7 @@ x1 = Clng(MyArray(0))   //窗口左上X
 y1 = Clng(MyArray(1))   //窗口左上y
 x2 = Clng(MyArray(2))   //窗口右下X
 y2 = Clng(MyArray(3))   //窗口右下y
+PB = 1   //课程类型标志：1必修，2选修
 
 Function findkeywords(keywords) //获取关键字坐标
     KeyPress "Esc", 1
@@ -24,7 +25,7 @@ Function findkeywords(keywords) //获取关键字坐标
     KeyPress "Esc", 1
 End Function
 
-Function pageloading(loadingkeywords) //页面是否完成加载，返回值1为未正常加载
+Function pageloading(loadingkeywords) //页面是否完成加载，返回值1为未正常加载???
 	Delay 30000
 	Call findkeywords(loadingkeywords)
     If intX > 0 and intY > 0 Then
@@ -40,27 +41,68 @@ Function backtopage() //返回上一页
     Delay 10000
 End Function
 
-Rem quxuexi  //标记
-Delay 500
+Function findkeig(keiglwxk) 
+    Call findkeywords("查看详情")
+    Call findkeywords(keiglwxk)
+    If intX > 0 And intY > 0 Then 
+        MoveTo intX + 71, intY + 97
+        Delay 500
+        LeftClick 1
+        Goto keigxrze
+    Else 
+        KeyPress 116, 1
+        Delay 10000
+        Goto keiglwxk
+    End If
+End Function
+
+Rem Main
+Call findkeywords("所属教学计划")
+If intX > 0 and intY > 0 Then
+    Goto keiglwxk
+End If 
+Call findkeywords("上课时间")
+If intX > 0 and intY > 0 Then
+    Goto keigxrze
+End If 
+Call findkeywords("视频详情")
+If intX > 0 and intY > 0 Then
+    Goto xtxijpmm
+End If 
+
+Rem keiglwxk
+If PB = 1 Then
+    GJ = "线上课程【必修课】"
+Else
+    GJ = "线上课程【选修课】"
+End If
+Call findkeig(GJ)
+
+Rem keigxrze
+P = pageloading("视频")
+If P = 0 Then
+	KeyPress 116, 1  
+	Goto keigxrze
+End If
 Call findkeywords("去学习")
 If intX < 0 And intY < 0 Then 
-    P = pageloading("视频")
-    If P Then
-    	KeyPress 116, 1  
-    	Delay 30000
-    	Goto quxuexi
+    If PB = 1 Then
+        PB = 2
     Else 
-    	Goto quxuexi
-	End If
+        PB = 1
+    End If 
+    Call backtopage()
+    Delay 10000
+    Goto keiglwxk
 End If
-
 MoveTo intX+5, intY+5
 LeftClick 1
-P = pageloading("视频加载中")
+
+Rem xtxijpmm
+P = pageloading("视频加载中")  //偶尔没有出现"视频加载中"
 IF P Then
-	Call backtopage()
-	Delay 10000
-	goto quxuexi
+	KeyPress 116, 1  
+	Goto xtxijpmm
 Else
 	While True
     	Delay 60000
@@ -71,14 +113,12 @@ Else
         	LeftClick 1
         	Delay 1000
         	Call backtopage()
-        	Delay 10000
-        	Goto quxuexi
+        	Goto keigxrze
     	End If
     	Call findkeywords("该视频已学习时长")
     	IfColor intX+90, intY-83, "FFFFFF", 0 Then
             Call backtopage()
-            Delay 10000
-            Goto quxuexi
+            Goto keigxrze
     	End If
         Call findkeywords("缓冲中")
         If intX > 0 And intY > 0 Then 
@@ -86,30 +126,15 @@ Else
             Call findkeywords("缓冲中")
             If intX > 0 And intY > 0 Then 
                 Call backtopage()
-                Delay 10000
-                Goto quxuexi
+                Goto keigxrze
             End If
         End If
     	Delay 500
     	KeyPress "Up", 1
 	Wend
 End If	
-
 Rem endstudy
 
 
-Function findkeig(keigxrze) //暂未使用，下一步优化时使用
-    findkeywords(keigxrze)
-    If intX > 0 And intY > 0 Then 
-        MoveTo intX + 71, intY + 97
-        Delay 500
-        LeftClick 1
-    Else 
-        //刷新页面
-    End If
-End Function
-//done
-//自定义函数运行 
 //todo
-//多页面执行
 //后台运行
