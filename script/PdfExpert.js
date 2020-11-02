d666bc9ebde3c0b9e0f26458f1ffa76ea7626158
allin_schroe <allin.schroe@gmail.com>
2020-11-02 22:18:28 +0800

    pdf
    self

---
+++ script/PdfExpert.js
@@ -0,0 +1,21 @@
+var obj= {
+  "receiptStatus" : "ok",
+  "subscriptionState" : "notActive",
+  "inAppStates" : [
+    {
+      "type" : "custom purchase",
+      "entitlements" : [
+        "ios.pe6.basic-features"
+      ],
+      "productId" : "pdfexpert6-user"
+    }
+  ],
+  "chargingPlatform" : "iOS AppStore",
+  "isEligibleForIntroPeriod" : true,
+  "isPDFExpert6User" : true,
+  "receiptId" : 1447477507000,
+  "bundleId" : "com.readdle.PDFExpert5"
+}
+;
+
+$done({body: JSON.stringify(obj)});
