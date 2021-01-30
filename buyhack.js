9da6fc048243337dfa66b83f7e232755a4dc630b
allin_schroe <allin.schroe@gmail.com>
2021-01-30 19:19:22 +0800

    fix

---
+++ buyhack.js
@@ -0,0 +1,4 @@
+hostname = p7-buy.itunes.apple.com
+
+#buyhack
+^https:\/\/p7-buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/abingx/Self-Rule/master/script/verify_receipt.js
