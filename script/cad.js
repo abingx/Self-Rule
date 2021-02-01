19a6f823dca0858263b48362df5b6154d0e297d4
allin_schroe <allin.schroe@gmail.com>
2021-02-01 22:38:47 +0800

    fix

--- script/cad.js
+++ script/cad.js
@@ -1,6 +1,25 @@
 var obj = JSON.parse($response.body);
-let obj.rs.vipInfo.serverEndTimeStamp = 1954815132000;
-let obj.rs.vipInfo.serverEndTime = "2031-12-12 12:12:12";
-let obj.rs.active.app_try_get_info.try_end_date = 1954815132000;
+
+let obj.rs.active.app_try_get_overdue= true;
+
+let obj.rs.active.app_try_get_info ={
+        "try_request_date" : 1612188533097,
+        "try_end_date" : 1954815132000,
+        "try_start_date" : 1612184998000
+      };
+let obj.rs.vipInfo = {
+      "serverEndTimeStamp" : 1954815132000,
+      "amount" : 0,
+      "appTryStatus" : true,
+      "serverEndTime" : "2031-12-12 12:12:12",
+      "goodId" : 73,
+      "canBuy" : false,
+      "canUpgrade" : false,
+      "goodsCodes" : "ML2",
+      "descs" : "免费领取高级账户1小时试用,不包含免广告和云图扩容",
+      "serverStartTime" : "2021-02-01 21:09:58",
+      "serverStartTimeStamp" : 1612184998000,
+      "name" : "高级账户"
+    }
 
 $done({ body: JSON.stringify(obj) });
