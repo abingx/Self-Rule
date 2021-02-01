4646a64d1bd3f66fcc408c522ffa48b39f34e9a5
allin_schroe <allin.schroe@gmail.com>
2021-02-01 21:31:59 +0800

    cad

---
+++ script/cad.js
@@ -0,0 +1,109 @@
+var obj= {
+  "code": "0",
+  "msg": "登录成功",
+  "rs": {
+    "active": {
+      "app_try_get_guide": false,
+      "app_try_get_id": 2,
+      "app_try_get_info": {
+        "try_end_date": 3821097600000,
+        "try_request_date": 1612185018648,
+        "try_start_date": 1612184998000
+      },
+      "app_try_get_overdue": true,
+      "app_try_get_show": true,
+      "app_try_get_usable": false,
+      "app_try_give_id": 9,
+      "app_try_give_show": false,
+      "app_try_give_usable": false
+    },
+    "advertisingUrl": "http://web.gstarcad.com/upgrade?uid=4321770&utoken=c5097f78-db72-4dd4-921f-24955e6e206c",
+    "chat": {
+      "ordinary": {
+        "createGroupNumberLimit": 3,
+        "createGroupUserNumberLimit": 200,
+        "joinGroupNumberLimit": 500
+      },
+      "senior": {
+        "createGroupNumberLimit": 20,
+        "createGroupUserNumberLimit": 200,
+        "joinGroupNumberLimit": 500
+      },
+      "userSig": "eJw9jsEKgkAURf9ltoW8N47OKLSwFjXRzgpmKTjG0wzRwbTo3xONlvdcDpw3O59Szw4NtZbFIQgFsJ5Zb1sWM*4BW3aXV1nTUM5iFAC*lFGEy0O5fTgqaBaEz1HKv0S3iWWDWo3ukmqjgu5gRFlqc**TJLma3YhPvn91x3qLVEm9*YmO6qkHQ*SoAkD1*QLuLTBF"
+    },
+    "myAmountNumbers": "2个群组，每个群组限5人",
+    "recentlyExpired": [],
+    "serverCode": "CN",
+    "serverLink": "http://cloud.gstarcad.com/getServerLink.do",
+    "serverUrl": "http://user.gstarcad.com/login",
+    "storage": {
+      "free": 100,
+      "surplus": 100,
+      "total": 100,
+      "used": 0
+    },
+    "third": [
+      {
+        "favicon": "http://thirdwx.qlogo.cn/mmopen/vi_32/xpzVGibPLxHospia3ckhDXYdbuH6SrHfmOSuRnIlOkwhqXSO01pGm0xN7NJb1IT358ibVElIwb7KoKgB0Za00WgjA/132",
+        "name": "薛",
+        "openId": "o7FOBxJg--LpI94-TVko8OFhQGpM",
+        "sex": "1",
+        "type": "weixin",
+        "unionId": "oKziUs7J6essD2uROYWmLkK6hrbc"
+      },
+      {
+        "favicon": "http://thirdqq.qlogo.cn/g?b=oidb&k=yDwXGFjPfE1Iw37wKX7IBw&s=100&t=1556716251",
+        "name": "薛",
+        "openId": "3A52512AD2B1D1FEE949FAEE5A0AD1F8",
+        "sex": "1",
+        "type": "qq",
+        "unionId": "UID_6C7BF5154B4A063C4F4503FD5295988C"
+      },
+      {
+        "favicon": "https://tva2.sinaimg.cn/crop.0.0.180.180.50/72e83832jw1e8qgp5bmzyj2050050aa8.jpg?KID=imgbed,tva&Expires=1575368382&ssig=J6t5pz0dnv",
+        "name": "冰哥很蛋定",
+        "openId": "1927821362",
+        "sex": "1",
+        "type": "weibo",
+        "unionId": ""
+      },
+      {
+        "type": "apple"
+      }
+    ],
+    "userInfo": {
+      "email": "allin_schroe@hotmail.com",
+      "favicon": "http://thirdwx.qlogo.cn/mmopen/vi_32/xpzVGibPLxHospia3ckhDXYdbuH6SrHfmOSuRnIlOkwhqXSO01pGm0xN7NJb1IT358ibVElIwb7KoKgB0Za00WgjA/132",
+      "id": 4321770,
+      "isActivate": 1,
+      "lastLoginTime": "2021-02-01 21:10:18",
+      "level": 1,
+      "mobile": "17761239071",
+      "name": "allin_schroe",
+      "nickName": "allin_schroe",
+      "registerDate": "2019-12-03 15:18:51",
+      "score": 6,
+      "sex": 1,
+      "updateTime": "2019-12-15 15:41:03"
+    },
+    "utoken": "c5097f78-db72-4dd4-921f-24955e6e206c",
+    "vipInfo": {
+      "amount": 0,
+      "appTryStatus": true,
+      "canBuy": false,
+      "canUpgrade": false,
+      "descs": "免费领取高级账户1小时试用,不包含免广告和云图扩容",
+      "goodId": 73,
+      "goodsCodes": "ML2",
+      "name": "高级账户",
+      "serverEndTime": "2091-02-01 22:09:58",
+      "serverEndTimeStamp": 3821097600000,
+      "serverStartTime": "2021-02-01 21:09:58",
+      "serverStartTimeStamp": 1612184998000
+    },
+    "vipList": []
+  },
+  "status": true
+};
+
+$done({body: JSON.stringify(obj)});
