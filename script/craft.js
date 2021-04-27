66f5339410ebdccc3d2b9910f7ae12bbcab05563
allin_schroe <allin.schroe@gmail.com>
2021-04-27 20:30:15 +0800

    craft

--- script/craft.js
+++ script/craft.js
@@ -1,4 +1,6 @@
 var obj = JSON.parse($response.body);
 let obj.subscription.expirationDate = 1890725831000;
+let obj.subscription.tier = year;
+let obj.subscription.subscriptionActive = true;
 
 $done({ body: JSON.stringify(obj) });
