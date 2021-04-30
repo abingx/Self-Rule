4b63f718ebe620f8df4de3a7e05d7605a358e841
abingx <allin.schroe@gmail.com>
2021-04-30 10:33:49 +0800

    fix

--- script/craft.js
+++ script/craft.js
@@ -1,6 +1,6 @@
 let obj = JSON.parse($response.body);
 obj.subscription.expirationDate = 1890725831000;
-obj.subscription.tier = "Pro";
+obj.subscription.tier = "year";
 obj.subscription.subscriptionActive = true;
 obj.isBetaUser = false;
 
