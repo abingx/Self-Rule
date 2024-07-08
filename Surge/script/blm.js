39a481932841767c278b9e1faa4716a8c26fdee3
bughub <allinschroe@gmail.com>
2024-07-08 20:25:27 +0800

    fix

--- Surge/script/blm.js
+++ Surge/script/blm.js
@@ -1,3 +1,5 @@
 let obj = JSON.parse($response.body);
-obj.user_infos.vip=true;
+if (Array.isArray(obj.user_infos) && obj.user_infos.length > 0) {
+  obj.user_infos[0].vip = true;
+}
 $done({body: JSON.stringify(obj)});
