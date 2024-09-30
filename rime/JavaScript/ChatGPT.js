7ce7db4cca40d060e2d719a721428072fba1d4ce
Bing <allin.schroe@gmail.com>
2024-09-30 14:27:32 +0800

    rime update

--- rime/JavaScript/ChatGPT.js
+++ rime/JavaScript/ChatGPT.js
@@ -4,28 +4,27 @@ async function aiChat() {
   const model = "o1-mini";
   const prompt = "你是一位 AI 助手，能够回答得专业且准确";
   const question = $searchText || $pasteboardContent;
-  const body = {
-    model: model,
-    messages: [
-      { role: "system", content: prompt },
-      { role: "user", content: question }
-    ]
-  };
   try {
     const response = await $http({
       url: url,
       method: "POST",
-      header: { 
+      header: {
         "Authorization": `Bearer ${key}`,
         "Content-Type": "application/json"
       },
-      body: body,
+      body: {
+        model: model,
+        messages: [
+          { role: "system", content: prompt },
+          { role: "user", content: question }
+        ]
+      },
     });
     if (response.response.statusCode !== 200) {
       return "请求失败";
     }
     const jsonData = JSON.parse(response.data);
-    return jsonData.choices[0].message.content; 
+    return jsonData.choices[0].message.content;
   } catch (error) {
     $log(error);
     return "";
--- rime/JavaScript/网购评价（逆向千问）.js
+++ rime/JavaScript/网购评价（逆向千问）.js
@@ -2,15 +2,8 @@ async function aiChat() {
   const url = $qwen_url + "/v1/chat/completions";
   const key = $qwen_key;
   const model = "qwen-max";
-  const prompt = "你是我的购物评价助手，如果输入"产品名称/好评"，则输出一段好评；如果输入"产品名称/中评"，则输出一段中评；如果输入"产品名称/差评"，则输出一段差评。评价60-100字左右，内容侧重使用感受，通俗易懂口语化，可以按照1、2、3、来分段，并加入emoji表情。";
+  const prompt = "你是我的购物评价助手，如果输入“产品名称/好评”，则输出一段好评；如果输入“产品名称/中评”，则输出一段中评；如果输入“产品名称/差评”，则输出一段差评。评价60-100字左右，内容侧重使用感受，通俗易懂口语化，可以按照1、2、3、来分段，并加入emoji表情。";
   const question = $searchText || $pasteboardContent;
-  const body = {
-    model: model,
-    messages: [
-      { role: "system", content: prompt },
-      { role: "user", content: question }
-    ]
-  };
   try {
     const response = await $http({
       url: url,
@@ -19,7 +12,13 @@ async function aiChat() {
         "Authorization": `Bearer ${key}`,
         "Content-Type": "application/json"
       },
-      body: body,
+      body: {
+        model: model,
+        messages: [
+          { role: "system", content: prompt },
+          { role: "user", content: question }
+        ]
+      },
     });
     if (response.response.statusCode !== 200) {
       return "请求失败";
