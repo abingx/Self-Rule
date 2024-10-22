91df74c7b9709faf4874e6e8eebee3cac7701531
Bing <allin.schroe@gmail.com>
2024-10-22 10:11:23 +0800

    localapi fix

--- Surge/script/localapi.js
+++ Surge/script/localapi.js
@@ -1,4 +1,4 @@
-if ($network.wifi.ssid.includes("shzlk")) {
+if ($network && $network.wifi.ssid && $network.wifi.ssid.includes("shzlk")) {
     let url = $request.url;
     let headers = JSON.parse(JSON.stringify($request.headers));
     let body = $request.body;
