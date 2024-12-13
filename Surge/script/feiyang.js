713b8026aad19afb69ddddab7c6e87cc8acf455d
Sit <allinschroe@gmail.com>
2024-12-13 10:13:04 +0800

    feiyang fix

--- Surge/script/feiyang.js
+++ Surge/script/feiyang.js
@@ -7,12 +7,16 @@ let newUrl;
 if (wifiName) {
 	if (wifiName.includes("shzlk")) {
 		newUrl = `http://mac-mini.local:35455/${path}`;
+		headers.host = 'mac-mini.local';
 	} else if (wifiName.includes("H&B_Family")) {
 		newUrl = `http://xueimac.local:35455/${path}`;
+		headers.host = 'xueimac.local';
 	} else {
 		newUrl = `https://feiyang.allinhub.top/${path}`;
+		headers.host = 'feiyang.allinhub.top';
 	}
 } else {
 	newUrl = `https://feiyang.allinhub.top/${path}`;
+	headers.host = 'feiyang.allinhub.top';
 }
-$done({ url: newUrl });
+$done({ url: newUrl, headers: headers });
