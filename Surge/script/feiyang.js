93cddbb20e04dc5c6af107fde8df34d291e4a040
Sit <allinschroe@gmail.com>
2024-12-13 10:24:15 +0800

    feiyang fix

--- Surge/script/feiyang.js
+++ Surge/script/feiyang.js
@@ -1,22 +1,21 @@
 const wifiName = $network.wifi.ssid;
 let url = $request.url;
 let headers = JSON.parse(JSON.stringify($request.headers));
-let body = $request.body;
 let path = url.match(/^http:\/\/feiyang\.allinone\/(.*)$/)[1];
 let newUrl;
 if (wifiName) {
 	if (wifiName.includes("shzlk")) {
 		newUrl = `http://mac-mini.local:35455/${path}`;
-		headers.host = 'mac-mini.local';
+		headers.Host = 'mac-mini.local';
 	} else if (wifiName.includes("H&B_Family")) {
 		newUrl = `http://xueimac.local:35455/${path}`;
-		headers.host = 'xueimac.local';
+		headers.Host = 'xueimac.local';
 	} else {
 		newUrl = `https://feiyang.allinhub.top/${path}`;
-		headers.host = 'feiyang.allinhub.top';
+		headers.Host = 'feiyang.allinhub.top';
 	}
 } else {
 	newUrl = `https://feiyang.allinhub.top/${path}`;
-	headers.host = 'feiyang.allinhub.top';
+	headers.Host = 'feiyang.allinhub.top';
 }
 $done({ url: newUrl, headers: headers });
