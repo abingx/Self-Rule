4b60390233e488d194fdb6b8f0ef3de7c1e2b90c
Sit <allinschroe@gmail.com>
2024-12-13 09:56:24 +0800

    feiyang fix

--- Surge/script/feiyang.js
+++ Surge/script/feiyang.js
@@ -2,25 +2,17 @@ const wifiName = $network.wifi.ssid;
 let url = $request.url;
 let headers = JSON.parse(JSON.stringify($request.headers));
 let body = $request.body;
-let match = url.match(/^http:\/\/feiyang\.allinone\/(.*)$/);
-if (!match) {
-	$done({});
-} else {
-	let path = match[1];
-	let newUrl;
-	if (wifiName) {
-		if (wifiName.includes("shzlk")) {
-			newUrl = `http://Mac-mini.local:35455/${path}`;
-		} else if (wifiName.includes("H&B_Family")) {
-			newUrl = `http://xueiMac.local:35455/${path}`;
-		} else {
-			// 阻止访问，返回空的响应
-			$done({});
-		}
+let path = url.match(/^http:\/\/feiyang\.allinone\/(.*)$/)[1];
+let newUrl;
+if (wifiName) {
+	if (wifiName.includes("shzlk")) {
+		newUrl = `http://mac-mini.local:35455/${path}`;
+	} else if (wifiName.includes("H&B_Family")) {
+		newUrl = `http://xueimac.local:35455/${path}`;
 	} else {
-		// 没有 Wi-Fi 名称时也阻止访问
-		$done({});
+		newUrl = `https://feiyang.allinhub.top/${path}`;
 	}
-	// 返回修改后的 URL
-	$done({ url: newUrl });
+} else {
+	newUrl = `https://feiyang.allinhub.top/${path}`;
 }
+$done({ url: newUrl });
